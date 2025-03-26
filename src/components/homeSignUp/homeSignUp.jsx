ae1b868221af483ce1b96389e352037f2e73313e
boluwaatife ajijola <ajijolanurudeen@gmail.com>
2025-03-26 06:23:23 +0100

    fixing routes and updating pictures

--- src/components/homeSignUp/homeSignUp.jsx
+++ src/components/homeSignUp/homeSignUp.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './homeSignUp.css'
+import { Link } from 'react-router-dom';
 export default function HomeSignUp(){
 
     return(
         <div className="HomeSignUp">
             <h1 className="HomeSignUpTexth1">Join the Meat Sharing Community Today</h1>
             <p className="HomeSignUpTextp">Sign Up now to enjoy exclusive deals and also meet other meat sharing enthusiasts</p>
-            <button>Sign Up</button>
+            <button> <Link to = "/SignUp">Sign Up</Link></button>
         </div>
     )
 }
