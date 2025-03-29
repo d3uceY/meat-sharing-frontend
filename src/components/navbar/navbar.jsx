72ab3b8e7f8b35c932e393b4c8fb503c03675a07
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-03-29 12:53:41 +0100

    @update/installed tailwind

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,37 +2,36 @@ import React from 'react'
 import './navbar.css'
 import { Routes, Route, Link } from "react-router-dom";
 import InfoSection from '../infoSection/infoSection';
-import Footer from '../footer/footer'; 
+import Footer from '../footer/footer';
 import logo from "/assets/Group\ 4.png";
 
-export default function Navbar({scrollToInfo,scrollToFooter}){
-    return(
+export default function Navbar({ scrollToInfo, scrollToFooter }) {
+    return (
         <div>
 
-        <nav className='nav'>
-            <div className='logo-section'>
-                 <img src= {logo} alt= " Protein logo" /> 
-            </div>
-            <div className='main-nav'>
-                <ul>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#" onClick={(e)=>{e.preventDefault(); scrollToInfo();}}>About Us</a></li>
-                    <li><a href="#" onClick={(e)=>{e.preventDefault(); scrollToFooter();}}>Contact Us</a></li>
-                </ul>
-            </div>
-            <div className='nav-utils'>
-                <ul>
-                    <li><Link to ="./SignUp">Sign In</Link></li>
-                    <li><Link to ="./cart">Cart</Link></li>
+            <nav className='nav'>
+                <div className='logo-section'>
+                    <img src={logo} alt=" Protein logo" />
+                </div>
+                <div className='main-nav'>
+                    <ul>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#" onClick={(e) => { e.preventDefault(); scrollToInfo(); }}>About Us</a></li>
+                        <li><a href="#" onClick={(e) => { e.preventDefault(); scrollToFooter(); }}>Contact Us</a></li>
+                    </ul>
+                </div>
+                <div className='nav-utils'>
+                    <ul>
+                        <li><Link to="./SignUp">Sign In</Link></li>
+                        <li><Link to="./cart">Cart</Link></li>
+                    </ul>
+                </div>
+            </nav>
 
-                </ul>
-            </div>
-        </nav>
-
-          {/* <Routes>
+            {/* <Routes>
           <Route path="/infoSection" element={<InfoSection />} />
           <Route path="/footer" element={<Footer />} />
          </Routes> */}
-         </div>
+        </div>
     )
 }
